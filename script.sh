63a6dd469b40182afb08756f6fa700e61fa204b1
Paul Feitzinger <paul@pfeyz.com>
2018-02-06 15:26:39 -0500

    drop random scripts and csvs

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	python main1.py 3 6 $line 
-done< "$1"
--- script1.sh
+++
@@ -1,6 +0,0 @@
-while IFS='' read -r line || [[ -n "$line" ]]; do
-        read -r line2
-        python main1.py 3 6 $line &
-        python main1.py 3 6 $line2 &
-        wait
-done < "$1"
